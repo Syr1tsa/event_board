958ffe8b0ad4193c3676e863cf71f5f5fda2f6ff
syr1tsa <syr1tsa9@yandex.ru>
2020-09-18 23:03:23 +0300

    first alpha version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   REGEX_FOR_EMAIL = /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
   has_many :events
@@ -6,4 +10,12 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 35}
   validates :email, uniqueness: true, presence: true, length: {maximum: 255},
             format: REGEX_FOR_EMAIL
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Товарищ #{rand(777)}" if self.name.blank?
+  end
 end
