0aba16a7dc60331b8e1e429f24006e9673c79f1e
syr1tsa <syr1tsa9@yandex.ru>
2020-09-23 02:15:06 +0300

    added comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates_presence_of :title, :address, :datetime, :user
   validates :title, length: {maximum: 255}
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   REGEX_FOR_EMAIL = /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
   has_many :events
+  has_many :comments
 
   validates :name, presence: true, length: {maximum: 35}
   validates :email, uniqueness: true, presence: true, length: {maximum: 255},
